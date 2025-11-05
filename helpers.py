743db82a4e9d098874896ae8357d6dd8f905497b
Matthew <matkit27@gmail.com>
2025-11-05 23:21:26 +0200

    WriteBitSequence

--- helpers.py
+++ helpers.py
@@ -1,5 +1,7 @@
 import os
 import copy
+from typing import Iterable
+
 
 class ReadBitStream:
     def __init__(self, data: bytes | bytearray):
@@ -42,8 +44,8 @@ class ReadBitStream:
         if self.eos or (self.i + 8*self.j + 8 > self.N):
             raise RuntimeError("EOS")
         
-        first = (self.data[self.j] >> self.i)
-        second = (self.data[self.j] << (8 - self.i))
+        first = (self.data[self.j] >> self.i) % 256
+        second = (self.data[self.j] << (8 - self.i)) % 256
 
         self._next(8)
         
@@ -79,33 +81,51 @@ class WriteBitStream:
         pass
 
 
-    def _next(self, k: int = 1):
-        self.j += ((self.i + k) // 8)
-        self.i = (self.i + k) % 8
-        
-        if self.i + 8*self.j >= self.N:
-            self.eos = True
+
+    def get_data(self):
+        return bytes(self.data)
 
 
-    def read_bit(self):
-        if self.eos:
-            raise RuntimeError("EOS")
+    def write_bit(self, b: bool):
+        if (b != 0) and (b != 1):
+            raise RuntimeError("b must be a bit (0 or 1)")
         
-        b = bool(self.data[self.j] & self._masks[self.i])
-        self._next()
-        
-        return b
+        if self.i == 0:
+            self.data.append(0)
 
-    def read_byte(self):
-        if self.eos or (self.i + 8*self.j + 8 > self.N):
-            raise RuntimeError("EOS")
-        
-        first = (self.data[self.j] >> self.i)
-        second = (self.data[self.j] << (8 - self.i))
+        self.data[-1] = (self.data[-1] | (b << self.i))
+        self.i = (self.i + 1) % 8
 
-        self._next(8)
+
+    def write_bits(self, bits: Iterable[bool]):
+        for b in bits:
+            self.write_bit(b)
         
-        return (first | second)
+
+    def write_byte(self, b: int | bytes):
+        if isinstance(b, int):
+            if (b > 255) or (b < 0):
+                raise RuntimeError("Byte must be in range [0, 255]")
+            bm = b
+        elif isinstance(b, bytes):
+            if len(b) != 1:
+                raise RuntimeError("b must be a single byte")
+            bm = b[0]
+        else:
+            raise RuntimeError("b must be of type bytes or int")
+        
+        self.data.append(0)
+        
+        if self.i == 0:
+            self.data[-1] = bm
+        else:
+            self.data[-2] = (self.data[-2] | ((bm << self.i) % 256))
+            self.data[-1] = (self.data[-1] | ((bm >> (8 - self.i)) % 256))
+
+
+    def write_bytes(self, B: bytes | bytearray):
+        for b in B:
+            self.write_byte(b)
 
 
 def read_bin_file_data(filepath: str) -> bytes:
