ee9a35d1584966ddd7ebc8aa17fb64d5fe291f5a
Matthew <matkit27@gmail.com>
2025-12-02 22:45:17 +0200

    some cumit

--- LZW.py
+++ LZW.py
@@ -21,8 +21,6 @@ class IRWayTrie:
         self.root.children = {i: IRWayTrie.IRWayTrieNode(i) for i in range(256)}
         self.n_phrases = 256
 
-        self.max_dict_size = MAX_DICT_SIZE
-
         self.current_node = self.root
 
     def next(self, char: int):
@@ -35,7 +33,7 @@ class IRWayTrie:
             return None
         
         else:
-            if self.n_phrases < self.max_dict_size:
+            if self.n_phrases < MAX_DICT_SIZE - 1:
                 self.current_node.add_child(char, self.n_phrases)
                 self.n_phrases += 1
             idx = self.current_node.index
@@ -57,10 +55,9 @@ class IDict:
             phrase = self.prev_phrase + bytes([self.prev_phrase[0]])
         
         else:
-            print(index, len(self.phrases))
             raise ValueError("Invalid index")
         
-        if not (self.prev_phrase is None) and (len(self.phrases) < MAX_DICT_SIZE):
+        if not (self.prev_phrase is None) and (len(self.phrases) < MAX_DICT_SIZE-1):
             self.phrases.append(self.prev_phrase + bytes([phrase[0]]))
         
         self.prev_phrase = phrase
@@ -79,10 +76,10 @@ def lzw_compress(data: bytes) -> bytes:
         index_bit_length = (trie.n_phrases - 2).bit_length()
 
         if not (res is None):
+            # print(f"Writing index ({index_bit_length}-bit):  {res}")
             output.write_bits(
                 int_to_bits(res, index_bit_length)
             )
-
 
     
     if trie.current_node.index is not None:
@@ -103,8 +100,10 @@ def lzw_decompress(data: bytes) -> bytes:
 
     while not (input.remaining_bits() < index_bit_length):
         index = bits_to_int(input.read_bits(index_bit_length))
+        # print(f"Reading index ({index_bit_length}-bit):  {index}")
         phrase = idict.next(index)
         index_bit_length = (len(idict.phrases)).bit_length()
+        # print(f"Writing phrase:  {phrase}")
         output.write_bytes(phrase)
 
     
